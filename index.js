17af7fd13d7ab331198dfbdf08ddf963103a6926
Piotr Kabaciski <piotr.kabacinski@gmail.com>
2017-09-07 07:44:00 +0200

    example es6 fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const gates = require("./dist/gates.min.js"),
+const gates = require('./dist/gates.min.js'),
       request = require('request'),
       express = require('express'),
       fs = require('fs'),
@@ -16,24 +16,24 @@ app.get('/mock', function(request, response) {
         },
         status = httpResponses[Math.floor(Math.random() * httpResponses.length)];
 
-    console.log("Response: " + status, data.foo);
+    console.log(`Response: ${status}, ${data.foo}`);
 
     response.status(status).send(data);
 
 });
 
-app.listen('8080', () => console.log('http://localhost:8080/'));
+app.listen('8080', () => { console.log('http://localhost:8080/'); });
 
 request('http://localhost:8080/mock', (error, response, body) => {
 
     body = JSON.parse(body);
 
     new gates().set(response.statusCode)
-        .gate([200, !body.foo], () => console.log("Hello 200 callback and false foo!"))
-        .gate([200, body.foo], () => console.log("Hello 200 callback and true foo!"))
-        .gate([404, "*"], () => console.log("Hello 404 callback and whatever foo!"))
-        .gate([503, body.foo], () => console.log("Hello 503 callback and true foo!"))
-        .gate(["*", "*"], () => console.log("Hello whatever callback!"))
-        .default(() => console.log("Hello default callback!"));
+        .gate([200, !body.foo], () => { console.log('Hello 200 callback and false foo!'); } )
+        .gate([200, body.foo], () => { console.log('Hello 200 callback and true foo!'); })
+        .gate([404, '*'], () => { console.log('Hello 404 callback and whatever foo!'); })
+        .gate([503, body.foo], () => { console.log('Hello 503 callback and true foo!'); })
+        .gate(['*', '*'], () => { console.log('Hello whatever callback!'); })
+        .default(() => { console.log('Hello default callback!'); });
 
 });
